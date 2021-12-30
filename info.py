20a489eb5f663a771cc6e0dd24f1a60bac4484f9
Asikson <asiamielniczuk11@gmail.com>
2021-12-30 17:07:58 +0100

    improving info

--- info.py
+++ info.py
@@ -1,5 +1,7 @@
 tablesInfo = dict()
+relations = dict()
 
+# tables' structure
 tablesInfo['employees'] = ('emp_id', 
     ['first_name',
     'last_name',
@@ -24,7 +26,19 @@ tablesInfo['salaries'] = ('id',
 tablesInfo['dept_manager'] = ('id',
     ['dept_id',
     'emp_id'])
-    
+
+# relations
+relations[('employees', 'emp_id')] = [
+    ('dept_emp', 'emp_id'),
+    ('dept_manager', 'emp_id'),
+    ('salaries', 'emp_id')]
+
+relations[('departments', 'dept_id')] = [
+    ('dept_emp', 'dept_id')]
+
+relations[('cities', 'city_id')] = [
+    ('departments', 'city_id')]
+
 
 def getTablePk(tablename):
     return tablesInfo[tablename][0]
@@ -32,3 +46,11 @@ def getTablePk(tablename):
 def getTableColumns(tablename):
     return [getTablePk(tablename)] + \
         tablesInfo[tablename][1]
+
+def isForeinKey(tablename1, field1, tablename2, field2):
+    return getTablePk(tablename2) == field2 \
+        and (tablename2, field2) in relations.keys() \
+        and (tablename1, field1) in relations[(tablename2, field2)]
+
+def isColumnInTable(column, tablename):
+    return column in getTableColumns(tablename)
